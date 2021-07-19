04674298cc2142a455a783912a3372585efcbc14
Fernando Issao Kassa <fernandokassa@yahoo.com.br>
2021-07-19 08:44:23 -0300

    Intro for arrays - 2 modes to initialize them

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,25 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            /*ContaCorrente[] contas = new ContaCorrente[3];
+            contas[0] = new ContaCorrente(1234, 123456);
+            contas[1] = new ContaCorrente(4321, 431276);
+            contas[2] = new ContaCorrente(1234, 542354);*/
+
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                new ContaCorrente(1234, 123456),
+	            new ContaCorrente(4321, 431276),
+	            new ContaCorrente(1234, 542354),
+            };
+            
+
+            for (int i = 0; i < contas.Length; i++)
+            {
+                ContaCorrente contaAtual = contas[i];
+                Console.WriteLine($"Conta {i} {contaAtual.Numero}");
+            }
+
             Console.ReadLine();
         }
     }
