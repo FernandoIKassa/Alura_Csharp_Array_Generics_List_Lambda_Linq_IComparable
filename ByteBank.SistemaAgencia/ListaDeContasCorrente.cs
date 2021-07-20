56c5606df82e5b0a2026c762c10849ad54038718
Fernando Issao Kassa <fernandokassa@yahoo.com.br>
2021-07-20 09:36:06 -0300

    Use of named arguments and optional arguments

---
+++ ByteBank.SistemaAgencia/ListaDeContasCorrente.cs
@@ -0,0 +1,50 @@
+﻿using ByteBank.Modelos;
+using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class ListaDeContasCorrente
+    {
+        private ContaCorrente[] _contas;
+        /// <summary>
+        /// Próxima posicao do array em que pode-se adicionar um item
+        /// </summary>
+        private int _proximaPosicao;
+        public ListaDeContasCorrente(int capacidadeInicial = 5)
+        {
+            _contas = new ContaCorrente[5];
+            _proximaPosicao = 0;
+        }
+
+        public void Adicionar(ContaCorrente contaASerAdicionada)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+            Console.WriteLine($"Adicionando no índice {_proximaPosicao} conta {contaASerAdicionada.Agencia}/{contaASerAdicionada.Numero}");
+            _contas[_proximaPosicao] = contaASerAdicionada;
+            _proximaPosicao++;
+        }
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if (_contas.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+            int novoTamanho = _contas.Length * 2;
+            Console.WriteLine("Aumentando capacidade da lista!");
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
+            for (int i = 0; i < _contas.Length; i++)
+            {
+                novoArray[i] = _contas[i];
+            }
+
+            _contas = novoArray;
+        }
+
+    }
+}
--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,12 +12,34 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            ListaDeContasCorrente lista = new ListaDeContasCorrente();
+            lista.Adicionar(new ContaCorrente(1234, 12345));
+            lista.Adicionar(new ContaCorrente(1234, 43215));
+            lista.Adicionar(new ContaCorrente(1234, 85674));
+            lista.Adicionar(new ContaCorrente(1234, 43215));
+            lista.Adicionar(new ContaCorrente(1234, 85674));
+            lista.Adicionar(new ContaCorrente(1234, 43215));
+            lista.Adicionar(new ContaCorrente(1234, 85674));
+            lista.Adicionar(new ContaCorrente(1234, 43215));
+            lista.Adicionar(new ContaCorrente(1234, 85674));
+            lista.Adicionar(new ContaCorrente(1234, 12345));
+            lista.Adicionar(new ContaCorrente(1234, 43215));
+            lista.Adicionar(new ContaCorrente(1234, 85674));
+            lista.Adicionar(new ContaCorrente(1234, 43215));
+            lista.Adicionar(new ContaCorrente(1234, 85674));
+            lista.Adicionar(new ContaCorrente(1234, 43215));
+            lista.Adicionar(new ContaCorrente(1234, 85674));
+            lista.Adicionar(new ContaCorrente(1234, 43215));
+            lista.Adicionar(new ContaCorrente(1234, 85674));
+            lista.Adicionar(new ContaCorrente(1234, 12345));
+
+
             /*ContaCorrente[] contas = new ContaCorrente[3];
             contas[0] = new ContaCorrente(1234, 123456);
             contas[1] = new ContaCorrente(4321, 431276);
             contas[2] = new ContaCorrente(1234, 542354);*/
 
-            ContaCorrente[] contas = new ContaCorrente[]
+            /*ContaCorrente[] contas = new ContaCorrente[]
             {
                 new ContaCorrente(1234, 123456),
 	            new ContaCorrente(4321, 431276),
@@ -29,7 +51,7 @@ namespace ByteBank.SistemaAgencia
             {
                 ContaCorrente contaAtual = contas[i];
                 Console.WriteLine($"Conta {i} {contaAtual.Numero}");
-            }
+            }*/
 
             Console.ReadLine();
         }
